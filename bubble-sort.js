dcd3f85d816b95ea66e8a93dbb16fd2e972e6de7
Jeff Lewis <jeffylew77@gmail.com>
2018-10-23 00:44:46 -0700

    [Dev] Adding Bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -3,6 +3,21 @@ Notes:
 - 
 */
 
+// Swap Numbers
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+// Bubble Sort
 function bubbleSort(array) {
-  
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 1; j < array.length; j++) {
+      if (array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
 }
