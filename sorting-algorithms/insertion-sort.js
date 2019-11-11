a4c2eded6f81652df13fa9787e8ebeb12bdbfe05
Jeff Lewis <jeffylew77@gmail.com>
2019-11-11 10:22:46 -0800

    [Dev] Insertion Sort working

--- sorting-algorithms/insertion-sort.js
+++ sorting-algorithms/insertion-sort.js
@@ -1,7 +1,9 @@
 /*
 Notes:
 - Works with unsorted arrays
-
+- Not Efficient
+- Time Complexity: O(n^2)	
+- Space Complexity: O(1)
 */
 
 /*
@@ -10,12 +12,26 @@ let array = [10, 4, 2, 5, 2, 1, 9, 0];
 */
 
 // Insertion Sort
-const insertionSort = () => {
+const insertionSort = (array) => {
   try {
-    //
+    // Iterate Over Array
+    for (let i = 0; i < array.length; i++) {
+      // Marker + Comparison Index
+      let marker = array[i];
+      let comparisonIndex = null;
 
+      // Iterate Over Array Backwards (J Must Be Greater or Equal To 0 And Greater Than The Marker)
+      for (comparisonIndex = i - 1; comparisonIndex >= 0 && array[comparisonIndex] > marker; comparisonIndex--) {
+        // Swap Index Values
+        array[comparisonIndex + 1] = array[comparisonIndex];
+      }
 
+      // What??
+      array[comparisonIndex + 1] = marker;
+    }
 
+    // Return Array
+    return array;
   }
   catch (error) {
     console.log(error);
