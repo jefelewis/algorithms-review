e196ca3cb148a972f6dfa63f9d1b397a3bc448f3
Jeff Lewis <jeffylew77@gmail.com>
2019-11-10 16:44:49 -0800

    [Dev] Adding Insertion Sort file

---
+++ sorting-algorithms/insertion-sort.js
@@ -0,0 +1,23 @@
+/*
+Notes:
+- Works with unsorted arrays
+
+*/
+
+/*
+Example:
+let array = [10, 4, 2, 5, 2, 1, 9, 0];
+*/
+
+// Insertion Sort
+const insertionSort = () => {
+  try {
+    //
+
+
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
