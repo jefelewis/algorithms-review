210615fde1817f2f73f308c88308f752ac184968
Jeff Lewis <jeffylew77@gmail.com>
2019-11-11 18:24:38 -0800

    [Dev] Adding skeleton for Heap Sort

---
+++ sorting-algorithms/heap-sort.js
@@ -0,0 +1,20 @@
+/*
+Notes:
+- Works with unsorted arrays
+*/
+
+/*
+Example:
+let array = [10, 4, 2, 5, 2, 1, 9, 0];
+*/
+
+
+// Heap Sort
+const heapSort = (array) => {
+  try {
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
